6ef9129575d557b0f40807f1b220090209f08bc8
Tim Sherry <tsherry82@gmail.com>
2019-06-27 19:09:36 -0400

    added alphabet array

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,7 @@
 // variables used for gameplay
-var words = ["Rattlesnake", "Saloon", "Revolver"]
-var guesses = 0;
+var words = ["Rattlesnake", "Saloon", "Revolver"];
+var guesses = 6;
+var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 
 
 var challenge = words[Math.floor(Math.random() * words.length)];
@@ -18,4 +19,12 @@ var remainingLetters = challenge.length;
 document.onkeyup = function(event) {
     
     var userGuess = event.key
+
+    for (var i = 0; i <challenge.length; i++) {
+        if (challenge[i] ===userGuess) {
+          answerArray[i] = userGuess;
+        }
+     
+        document.getElementById("hangman").innerHTML = answerArray.join(" ");
+      }
 }
